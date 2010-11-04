45450b23a7a888b4cecaa8202c3ce779d0b83b74
Bruno <bruno.engelbert@gmail.com>
2010-11-04 10:49:27 -0200

    adicionando arquivo fontes novos

---
+++ tcc/manager/dbfc.h
@@ -0,0 +1,18 @@
+/**
+ * @file    dbfc.h
+ */
+
+#ifndef _DBFC_H
+#define _DBFC_H
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _DBFC_H */
---
+++ tcc/manager/host.h
@@ -0,0 +1,18 @@
+/**
+ * @file    host.h
+ */
+
+#ifndef _HOST_H
+#define _HOST_H
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _HOST_H */
