6464b058080f224dcdc5443bd5105f7780226552
Bruno <bruno.engelbert@gmail.com>
2010-11-16 15:17:04 -0200

    adicionando fonte scripts

---
+++ tcc/manager/script.h
@@ -0,0 +1,19 @@
+/**
+ * @file    script.h
+ */
+
+#ifndef _SCRIPT_H
+#define _SCRIPT_H
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+#include "wrapper.h"
+    
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _SCRIPT_H */
